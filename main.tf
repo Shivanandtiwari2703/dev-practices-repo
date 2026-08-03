6bace5c5334f1d07adba951d50fe975a42ed9f19
shivanandtiwari2703 <shivanandtiwari2703@gmail.com>
2026-08-03 21:25:40 +0530

    rgs

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "5.00.0"
+    }
+  }
+}
+provider "azurerm" {
+    features {}
+}
+resource "azurerm_resource_group" "rg" {
+name = "my-rg"
+location = "centralindia"
+  
+}
